8d50b271081cd016982fca38a03f156eda93152c
Zastarech <decherflouis@gmail.com>
2016-06-14 16:07:21 +0200

    moyenne et médiane fonctionnels

--- app/src/main/java/project/informathique/Statistiques.java
+++ app/src/main/java/project/informathique/Statistiques.java
@@ -16,17 +16,19 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
-public class Statistiques extends Activity {
+public class Statistiques extends AppCompatActivity {
 
     private EditText editTxt;
     private String[] items = new String []{""};
     ListView mListView;
-    String[] nombres;
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
+    private float nombre = 0, total = 0;
 
 
     @Override
@@ -37,6 +39,9 @@ public class Statistiques extends Activity {
         mListView = (ListView) findViewById(R.id.listView);
         arrayList = new ArrayList<>(Arrays.asList(items));
         adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_item,arrayList);
+        adapter.clear();
+        final TextView Result = (TextView)findViewById(R.id.Resultat);
+        int count = adapter.getCount();
 
         // on assigne l'adapter à notre list
         mListView.setAdapter(adapter);
@@ -60,13 +65,118 @@ public class Statistiques extends Activity {
                     int duration = Toast.LENGTH_SHORT;
 
                     Toast.makeText(context, text, duration).show();
-                    //System.exit(0);
                 }
                 else
                 {
                     //String newNumber = txtvalue;
                     adapter.add(txtvalue);
+                    value.getText().clear();
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
 
+        Button ViderListe = (Button) findViewById(R.id.EmptyList);
+        ViderListe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                adapter.clear();
+            }
+        });
+
+        Button boutonCalcMoyenne = (Button) findViewById(R.id.btnCalculMoyenne);
+        boutonCalcMoyenne.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                for (int i = 0; i < adapter.getCount(); i++)
+                {
+                    if(adapter.getCount() > 0 )
+                    {
+                        nombre = Float.parseFloat(adapter.getItem(i));
+                        total = total + nombre;
+                    }
+                    else
+                    {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Il n'y a aucune valeur dans la liste, le calcul est impossible !";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast.makeText(context, text, duration).show();
+                    }
+
+
+                }
+                if(total == 0)
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Il n'y a aucune valeur dans la liste, le calcul est impossible !";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast.makeText(context, text, duration).show();
+                    Result.setText("");
+                }
+                else
+                {
+                    float moyenne = total / adapter.getCount();
+                    Result.setText("Moyenne :" + moyenne);
+                    total = 0;
+                }
+            }
+        });
+
+        Button Mediane = (Button) findViewById(R.id.btnCalculMediane);
+        Mediane.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                for (int i = 0; i < adapter.getCount(); i++)
+                {
+                    if(adapter.getCount() > 0 )
+                    {
+                        //Collections.sort(arrayList);
+                        nombre = Float.parseFloat(adapter.getItem(i));
+                        total = total + nombre;
+                    }
+                    else
+                    {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Il n'y a aucune valeur dans la liste, le calcul est impossible !";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast.makeText(context, text, duration).show();
+                    }
+                }
+                if(adapter.getCount() > 0 )
+                {
+                    double mediane = ((adapter.getCount() + 1) / 2.0);
+                    Result.setText("Mediane :" + mediane);
+                }
+                else
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Il n'y a aucune valeur dans la liste, le calcul est impossible !";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast.makeText(context, text, duration).show();
+                    Result.setText("");
+                }
+            }
+        });
+
+        Button Mode = (Button) findViewById(R.id.btnCalculMode);
+        Mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    if (adapter.getCount() > 0) {
+                        //Collections.sort(arrayList,new Comparator<String>(){
+
+                        };
+
+                    } else {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Il n'y a aucune valeur dans la liste, le calcul est impossible !";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast.makeText(context, text, duration).show();
+                    }
                 }
             }
         });
